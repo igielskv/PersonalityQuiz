6f3250e75a27345918e508aa20740a1f59e4ea57
Manoli <igielskv@icloud.com>
2020-08-19 18:29:35 +0200

    Setting up IntroductionView.

---
+++ PersonalityQuiz/IntroductionView.swift
@@ -0,0 +1,50 @@
+//
+//  IntroductionView.swift
+//  PersonalityQuiz
+//
+//  Created by Manoli on 15/08/2020.
+//  Copyright © 2020 macForce.one. All rights reserved.
+//
+
+import SwiftUI
+
+struct IntroductionView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("🐶")
+                    .font(.system(size: 40.0))
+                Spacer()
+                Text("🐱")
+                    .font(.system(size: 40.0))
+            }
+            
+            Spacer()
+            
+            Text("Which Animal Are You?")
+                .font(.custom("Georgia", size: 30.0))
+            
+            Button(action: {}) {
+                Text("Begin Personality Quiz")
+                    .font(.system(size: 15.0))
+            }
+            
+            Spacer()
+            
+            HStack {
+                Text("🐰")
+                    .font(.system(size: 40.0))
+                Spacer()
+                Text("🐢")
+                    .font(.system(size: 40.0))
+            }
+        }
+        .padding(.horizontal, 20.0)
+    }
+}
+
+struct IntroductionView_Previews: PreviewProvider {
+    static var previews: some View {
+        IntroductionView()
+    }
+}
